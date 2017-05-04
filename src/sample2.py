c5e58c5f32226c02c1210d0cff8e38e22a42b2c1
Jay <jay_thakkar@isb.edu>
2017-05-04 16:40:27 +0530

    iterating over directory

--- src/sample2.py
+++ src/sample2.py
@@ -1,31 +1,35 @@
 import cv2
 import numpy as np
+import os
+import time
 
-def image_enhancement (original_image):
+def image_enhancement(original_image):
     kernel_sharpen_3 = np.array([[-1, -1, -1, -1, -1],
                                  [-1, 2, 2, 2, -1],
                                  [-1, 2, 8, 2, -1],
                                  [-1, 2, 2, 2, -1],
                                  [-1, -1, -1, -1, -1]]) / 8.0
-    large_image = cv2.resize(original_image, (0, 0), fx=4, fy=4, interpolation = cv2.INTER_CUBIC)
+    large_image = cv2.resize(original_image, (0, 0), fx=4, fy=4, interpolation=cv2.INTER_CUBIC)
     output_image = cv2.filter2D(large_image, -1, kernel_sharpen_3)
     return output_image
 
 # For B/W
 # image = cv2.imread("/home/jay/Pictures/bloomberg/sample_20170428-120928.png", 0)
-image = cv2.imread("/home/jay/Pictures/bloomberg/sample_20170428-120928.png")
-location = '/home/jay/Pictures/bloomberg/t1.png'
 
-cv2.imshow("original", image)
-ticker_img = image[382:989, 45:321] # Crop from x, y, w, h -> 100, 200, 300, 400
-ticker_img = image_enhancement(ticker_img)
+input_directory = "/home/jay/Pictures/bloomberg/03-05-2017/"
+output_directory = "/home/jay/Desktop/"
+
+for filename in os.listdir("/home/jay/Pictures/bloomberg/03-05-2017/"):
+    if filename.endswith(".png"):
+        print(os.path.join(input_directory, filename))
+        input_image = cv2.imread(os.path.join(input_directory, filename))
+        ticker_img = input_image[382:989, 45:321]  # Crop from x, y, w, h -> 100, 200, 300, 400
+        ticker_img = image_enhancement(ticker_img)
+        time_str = time.strftime("%Y%m%d-%H%M%S")
+        file_name = "ticker_" + filename + ".png"
+        location = os.path.join(output_directory, file_name)
+        cv2.imwrite(location, ticker_img)
+
 
 # For B/W
-#(thresh, im_bw) = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-#For writing
-cv2.imwrite(location, ticker_img)
-#cv2.imshow("ticker", ticker_img)
-cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
+# (thresh, im_bw) = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
